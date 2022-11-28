35bba4571f3c4e2d32f631e8ee432534f3388733
Destin Eva Dila Purnama Sari <destin.sari@suryapamenang.local>
2022-11-28 14:33:32 +0700

    commit

---
+++ src/components/About.vue
@@ -0,0 +1,108 @@
+<<<<<<< HEAD
+
+import MapContainer from './components/MapContainer.vue'
+
+<template>
+       <div id="app">
+    <div class="cell cell-map">
+      <MapContainer/>
+    </div>
+    
+  </div>
+      
+    
+</template>
+
+<style>
+  html, body {
+    height: 100%;
+    margin: 0;
+  }
+
+  #app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    height: 100%;
+    display: grid;
+    grid-template-columns: 100vh;
+    grid-auto-rows: 1fr;
+    grid-gap: 1rem;
+    padding: 1rem;
+    box-sizing: border-box;
+  }
+
+
+  .cell {
+    border-radius: 4px;
+    background-color: lightgrey;
+  }
+
+  .cell-map {
+    grid-column: 1;
+    grid-row-start: 1;
+    grid-row-end: 3;
+  }
+
+  .cell-edit {
+    grid-column: 2;
+    grid-row: 1;
+  }
+
+  .cell-inspect {
+    grid-column: 2;
+    grid-row: 2;
+  }
+=======
+
+import MapContainer from './components/MapContainer.vue'
+
+<template>
+       <div id="app">
+    <div class="cell cell-map">
+      <MapContainer/>
+    </div>
+    
+  </div>
+      
+    
+</template>
+
+<style>
+  html, body {
+    height: 100%;
+    margin: 0;
+  }
+
+  #app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    height: 100%;
+    display: grid;
+    grid-template-columns: 100vh;
+    grid-auto-rows: 1fr;
+    grid-gap: 1rem;
+    padding: 1rem;
+    box-sizing: border-box;
+  }
+
+
+  .cell {
+    border-radius: 4px;
+    background-color: lightgrey;
+  }
+
+  .cell-map {
+    grid-column: 1;
+    grid-row-start: 1;
+    grid-row-end: 3;
+  }
+
+  .cell-edit {
+    grid-column: 2;
+    grid-row: 1;
+  }
+
+  .cell-inspect {
+    grid-column: 2;
+    grid-row: 2;
+  }
+>>>>>>> 222b48a (windicss update)
+</style>
---
+++ src/main.js
@@ -0,0 +1,19 @@
+<<<<<<< HEAD
+import { createApp } from 'vue'
+import './style.css'
+import './main.css'
+import App from './App.vue'
+import 'windi.css'
+import 'virtual:windi.css'
+
+createApp(App).mount('#app')
+=======
+import { createApp } from 'vue'
+import './style.css'
+import './main.css'
+import App from './App.vue'
+import 'windi.css'
+import 'virtual:windi.css'
+
+createApp(App).mount('#app')
+>>>>>>> 222b48a (windicss update)
