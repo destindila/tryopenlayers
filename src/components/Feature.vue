c945a2696c476108d844d149f034c0b7f7a3b655
destineva <destindila@gmail.com>
2022-11-28 09:00:23 +0700

    Add files via upload
    windicss try

---
+++ src/components/Feature.vue
@@ -0,0 +1,43 @@
+<script setup>
+  
+</script>
+
+<template>
+  <section id="category" class="flex flex-col items-center py-32">
+    <h2 class="my-10 text-5xl md:text-6xl font-bold fill-gray-500">FEATURE</h2>
+    <div class="grid grid-cols-1 md:grid-cols-3 place-items-center">
+      <div v-for="category of categories" :key="category.title" class="fill-gray-500 grid place-items-center gap-5 text-center">
+        <div class="p-10">
+          <img src="data/maps4.PNG" class="w-full">
+        </div>
+        <h3 class="text-3xl font-bold">yeeaay</h3>
+        <p class="w-3/4">ini adalah fitur pertama</p>
+      </div>
+    </div>
+  </section>
+  <section class="grid grid-flow-col grid-cols-1 md:grid-cols-2 md:gap-10 items-center">
+    <div class="md:p-10">
+      <img src="data/maps3.PNG">
+    </div>
+    <div class="text-center flex flex-col items-center">
+      <h2 class="text-5xl md:text-6xl font-bold fill-gray-500">BLALALALAA</h2>
+      <div class="flex items-center gap-5">
+        <h2 class="text-5xl md:text-6xl font-bold fill-gray-500">yeaaaaay</h2>
+        <img src="data/maps4.PNG" class="w-9 md:w-12" alt="head2">
+      </div>
+      <p class="mt-8 md:mt-12 fill-gray-500 text-lg md:text-xl w-full md:w-[80%]">ini adalah fitur kedua.</p>
+    </div>
+  </section>
+</template>
+
+
+<style>
+fill-gray-500 {
+  color:rgba(107, 114, 128, var(--tw-fill-opacity));
+  fill: rgba(107, 114, 128, var(--tw-fill-opacity));
+}
+.fill-green-500 {
+  --tw-fill-opacity: 1;
+  fill: rgba(16, 185, 129, var(--tw-fill-opacity));
+}
+</style>
---
+++ src/components/Home.vue
@@ -0,0 +1,33 @@
+<script setup>
+  
+ 
+</script>
+
+<template>
+  <section id="home" class="grid place-items-center mt-20 md:mt-10 overflow-hidden">
+    <div class="grid grid-flow-row-dense grid-cols-1 lg:grid-cols-2 items-end gap-10 lg:gap-16 w-full px-0 lg:px-10">
+      <div class="text-5xl md:text-6xl font-bold fill-gray-500 w-full pb-20 order-2 lg:order-1">
+        <div class="flex items-center gap-3">
+          <h2>BOOO</h2>
+        </div>
+        <h2 class="text-4xl md:text-5xl">AKU PETAAA!!</h2>
+        <p class="pt-5 text-lg font-normal">blablabalalala . </p>
+      </div>
+      <div class="w-full h-full order-1 lg:order-2" style="width:600px">
+        <img src="data/maps2.PNG" >
+      </div>
+    </div>
+  </section>
+</template>
+
+
+<style>
+.fill-gray-500 {
+  color:rgba(107, 114, 128, var(--tw-fill-opacity));
+  fill: rgba(107, 114, 128, var(--tw-fill-opacity));
+}
+.fill-green-500 {
+  --tw-fill-opacity: 1;
+  fill: rgba(16, 185, 129, var(--tw-fill-opacity));
+}
+</style>
