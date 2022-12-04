37b1ff0a0fefa412c731af8161c245616d4a9c0e
destineva <destindila@gmail.com>
2022-12-04 17:51:40 +0700

    Add files via upload

--- windi.config.js
+++ windi.config.js
@@ -11,7 +11,7 @@ export default defineConfig({
         'md': '768px',
         'lg': '1024px',
         'xl': '1280px',
-        '2xl': '1536px', 
+        '2xl': '1536px',
       },
       colors: {
         blue: colors.sky,
